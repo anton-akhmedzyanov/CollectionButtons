693a5f8c5f0b2416be243c215111e5cb366c7b22
Anton Akhmedzyanov <anton.akhmedzyanov@gmail.com>
2023-05-06 00:24:41 +0300

    Initial Commit

---
+++ CollectionButtons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionButtons
+//
+//  Created by Anton Akhmedzyanov on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
